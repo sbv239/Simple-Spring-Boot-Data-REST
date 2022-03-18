93fa7d525298620f24c68c87fad383dbd1130ade
Boris <shramko.boris@gmail.com>
2022-03-18 11:49:09 +0300

    Version 1.0

---
+++ src/main/java/com/shramko/spring/springbot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.shramko.spring.springbot.spring_data_rest.dao;
+
+import com.shramko.spring.springbot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
